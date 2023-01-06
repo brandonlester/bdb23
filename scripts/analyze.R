fc54585af875e32a2e204f7fbd7a8dab3355ba0a
Brandon Lester <lesterbn20@gmail.com>
2023-01-05 21:26:53 -0500

    Adjusted anim & table with new results

--- scripts/analyze.R
+++ scripts/analyze.R
@@ -39,102 +39,71 @@ ol_positions <- hex_points[-1]
 
 # Read data ---------------------------------------------------------------
 
-#source data
-df_games <- read_csv(file.path(data_folder, "games.csv"))
-df_plays <- read_csv(file.path(data_folder, "plays.csv"))
+model_results <- readr::read_rds("data/model_results.rds")
+block_results <- model_results$block$df_all
+rush_results <- model_results$rush$df_all
+rm(model_results)
+gc()
+
+df_plays <- readr::read_csv(file.path(data_folder, "plays.csv"))
 df_players <- read_csv(file.path(data_folder, "players.csv"))
-df_pff <- read_csv(file.path(data_folder, "pffScoutingData.csv"))
 
 list_tracking <- readr::read_rds(file.path(data_folder, "list_tracking.rds"))
 df_tracking  <- bind_rows(list_tracking)
 rm(list_tracking)
-
-
-#results from model.R
-df_results_import <- readr::read_rds(file.path(output_folder, "df_results.rds"))
-df_results_player_import <- readr::read_rds(file.path(output_folder, "df_results_player.rds"))
+gc()
 
 # Analyze model results ---------------------------------------------------
 
 #skimr::skim(df_results)
 
-
-df_results <- df_results_import %>% 
-  arrange(gameId, playId, frameId) %>% 
-  group_by(gameId, playId) %>% 
-  mutate(
-    prev_pred_sack = lag(pred_sack),
-    start_sack_prob = pred_sack[frameId == min(frameId)]
-  ) %>% 
-  ungroup() %>% 
-  mutate(
-    start_probchange = pred_sack - start_sack_prob,
-    frame_probchange = pred_sack - prev_pred_sack,
-    spoe = sack - pred_sack
-  )
-
-
-
-df_results_player <- df_results_player_import %>% 
-  arrange(gameId, playId, frameId, nflId) %>% 
-  group_by(gameId, playId, nflId) %>% 
-  mutate(
-    prev_pred_sack = lag(pred_sack),
-    start_sack_prob = pred_sack[frameId == min(frameId)]
-  ) %>% 
-  ungroup() %>% 
-  mutate(
-    start_probchange = pred_sack - start_sack_prob,
-    frame_probchange = pred_sack - prev_pred_sack,
-    spoe = pff_sack - pred_sack
-  )
-
-
-df_trackwpreds <- df_tracking %>% 
-  inner_join(df_results, by = c("gameId", "playId", "frameId")) %>% 
-  left_join(df_results_player, by = c("gameId", "playId", "frameId", "nflId"), suffix = c(".play", ".def_player"))
-
-#df_trackwpred_player <- df_tracking %>% inner_join(df_results_player, by = c("gameId", "playId", "frameId", "nflId"))
-
-
-df_plays_wpreds <- df_results %>%
-  group_by(gameId, playId) %>% 
-  summarise(
-    start_sack_prob = unique(start_sack_prob),
-    end_sack_prob = pred_sack[frameId == max(frameId)],
-    sack = unique(sack),
-    across(
-      .cols = where(is.numeric), 
-      .fns = list(avg = ~mean(.x, na.rm = TRUE), sd = ~sd(.x, na.rm = TRUE)), 
-      .names = "{.fn}_{.col}"
-    ),
-    across(
-      .cols = c(pred_sack, pred_no_sack, spoe),
-      .fns = sum,
-      .names = "sum_{.col}"
+summarise_results <- function(df, target) {
+  names(df)[names(df) == target] <- "actual"
+  names(df)[names(df) == paste0("pred_", target)] <- "prob"
+  
+  df %>% 
+    arrange(gameId, playId, frameId, nflId) %>% 
+    group_by(gameId, playId, nflId) %>% 
+    mutate(
+      prev_prob = lag(prob),
+      start_prob = prob[frameId == min(frameId)]
+    ) %>% 
+    ungroup() %>% 
+    mutate(
+      start_probchange = prob - start_prob,
+      frame_probchange = prob - prev_prob,
+      ppoe = actual - prob
     )
-  ) %>% 
-  select(
-    -contains("frameId"), 
-    -c(
-      avg_sack, sd_sack, 
-      avg_start_sack_prob, sd_start_sack_prob,
-      avg_end_sack_prob, sd_end_sack_prob
-    )
-  ) %>% 
-  inner_join(df_plays, by = c("gameId", "playId"))
+}
 
+block_results_summary <- summarise_results(block_results, "pressure_allowed")
+rush_results_summary <- summarise_results(rush_results, "pressure_delivered")
+
+df_tracking %>% 
+  inner_join(select(block_results, gameId, playId, nflId, frameId), by = c("gameId", "playId", "frameId", "nflId")) %>% count(gameId, playId, frameId)
+
+
+df_results <- df_tracking %>% 
+  left_join(select(block_results, gameId, playId, frameId, nflId, pred_pressure_allowed), by = c("gameId", "playId", "frameId", "nflId")) %>%
+  left_join(select(rush_results, gameId, playId, frameId, nflId, pred_pressure_delivered), by = c("gameId", "playId", "frameId", "nflId")) %>% 
+  group_by(gameId, playId, frameId) %>% 
+  filter(any(!is.na(pred_pressure_allowed))) %>% 
+  filter(any(!is.na(pred_pressure_delivered))) %>% 
+  ungroup()
 
 # Visualize probability predictions ---------------------------------------
 
 
 #df_for_plot <- prep_example_for_plot(df_trackwpreds, 2021090900, 97) 
-df_for_plot <- prep_example_for_plot(df_trackwpreds, 2021091201, 63) %>% 
-  mutate(not_ball = team!="football")
+df_for_plot <- prep_example_for_plot(df_results, 2021091201, 63) %>% 
+  mutate(not_ball = team!="football") %>% 
+  mutate(prob = ifelse(!is.na(pred_pressure_allowed), pred_pressure_allowed, pred_pressure_delivered))
 
+#TODO add pocket size polygon
 pocket_plot <- ggplot(df_for_plot, aes(x = x, y = y)) + 
-  geom_point(aes(fill = pred_sack.def_player, size = not_ball), pch = 21) +
+  geom_point(aes(fill = prob, size = not_ball, color = team), pch = 21) +
   scale_fill_gradient(low = "white", high = "red") +
+  scale_color_manual(values = c("white", "grey", "black")) +
   geom_text(aes(x = x, y = y, label = jerseyNumber), color = "black", size = 3.5, vjust = 0.35) +
   ggtitle(df_for_plot$playDescription) +
   theme_minimal() +
@@ -151,6 +120,18 @@ pocket_plot <- ggplot(df_for_plot, aes(x = x, y = y)) +
 
 pocket_anim <- create_pocket_animation(pocket_plot, max(df_for_plot$frameId))
 
+ggsave(
+  path = paste0(output_folder,"/"),
+  filename = "draft_dots_plot.jpeg",
+  plot = pocket_plot,
+  device = jpeg,
+  units = "in",
+  width = 10,
+  height = 6,
+  dpi = 300
+)
+
+anim_save(animation = pocket_anim, filename = file.path(output_folder, "draft_dots_anim.gif"))
 
 # pocket_plot <- create_pocket_plot(df_for_plot) +
 #   geom_text(data = df_for_plot, aes(x = mean(x), y = max(y)+ 5, label = paste("Sack Probability", scales::percent(pred_sack.play, accuracy = 0.01), sep = ": ")))
@@ -180,38 +161,54 @@ pocket_anim <- create_pocket_animation(pocket_plot, max(df_for_plot$frameId))
 
 # Final metrics -----------------------------------------------------------
 
-spoe_rankings <- df_results_player %>% 
-  group_by(gameId, playId, nflId) %>% 
-  summarise(
-    sack_prob = unique(start_sack_prob),
-    sack = unique(pff_sack)
-  ) %>% 
-  ungroup() %>% 
-  mutate(spoe = sack - sack_prob) %>% 
-  group_by(nflId) %>% 
-  summarise(
-    snaps = n(),
-    total_spoe = sum(spoe),
-    avg_spoe = mean(spoe)
-  ) %>% 
-  arrange(desc(avg_spoe)) %>% 
-  inner_join(df_players, by = "nflId") %>% 
-  filter(snaps >= 100)
+ppoe_summary <- function(df, target) {
+  names(df)[names(df) == target] <- "actual"
+  names(df)[names(df) == paste0("pred_", target)] <- "prob"
+  
+  df %>% 
+    group_by(gameId, playId, nflId) %>% 
+    summarise(
+      prob = prob[frameId==min(frameId)],
+      actual = unique(actual)
+    ) %>% 
+    ungroup() %>% 
+    mutate(ppoe = actual - prob) %>% 
+    group_by(nflId) %>% 
+    summarise(
+      snaps = n(),
+      total_ppoe = sum(ppoe),
+      avg_ppoe = mean(ppoe)
+    ) %>% 
+    arrange(desc(avg_ppoe)) %>% 
+    inner_join(df_players, by = "nflId") %>% 
+    filter(snaps >= 100)
+}
 
-summary(spoe_rankings$snaps)
+block_ppoe <- ppoe_summary(block_results, "pressure_allowed")
+rush_ppoe <- ppoe_summary(rush_results, "pressure_delivered")
 
-spoe_table <- spoe_rankings %>% 
-  head(10) %>% 
-  mutate(
-    avg_spoe = round(avg_spoe, 3),
-    total_spoe = round(total_spoe, 3)
-  ) %>% 
-  select(
-    Name = displayName,
-    Position = officialPosition,
-    Snaps = snaps,
-    `Average SPOE` = avg_spoe,
-    `Total SPOE` = total_spoe
-  ) %>% 
-  kbl(caption = "Min 100 snaps") %>%
-  kable_paper("hover", full_width = FALSE)
+ppoe_table <- function(summary_ppoe) {
+  summary_ppoe %>% 
+    head(10) %>% 
+    mutate(
+      avg_ppoe = round(avg_ppoe, 3),
+      total_ppoe = round(total_ppoe, 3)
+    ) %>% 
+    select(
+      Name = displayName,
+      Position = officialPosition,
+      Snaps = snaps,
+      `Average PPOE` = avg_ppoe,
+      `Total PPOE` = total_ppoe
+    ) %>% 
+    kbl(caption = "Min 100 snaps") %>%
+    kable_paper("hover", full_width = FALSE)
+}
+
+block_ppoe_table <- ppoe_table(block_ppoe)
+rush_ppoe_table <- ppoe_table(rush_ppoe)
+
+#TODO - may not need to save to files, just display properly in kaggle notebook
+#save_kable(block_ppoe, "block_ppoe.jpeg")
+#webshot::install_phantomjs() #this got it to run
+#install.packages("magick") #suggest this but still html in jpeg as output
